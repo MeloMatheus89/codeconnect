7a0e2ab4848477365f19884ea9754161cc03818f
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-02-03 20:14:53 -0300

    adicionada barra de pesquisa com estilização e ícone

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 
 import './App.css'
+import BarraDePesquisa from './componentes/BarraDePesquisa'
 import Sidebar from './componentes/Sidebar'
 
 function App() {
   
 
   return (
+    <div>
     <Sidebar />
+    <BarraDePesquisa />
+    </div>
   )
 }
 
