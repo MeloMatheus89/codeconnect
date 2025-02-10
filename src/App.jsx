bc8a2d8bb34ae7985fa3f799eba58b1fa06387da
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-02-10 20:30:18 -0300

    lista criada e interagindo com o link externo do banco de dados

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,39 @@ import Sidebar from './componentes/Sidebar'
 import Filtro from './componentes/Filtros'
 import Ordenacao from './componentes/Ordenacao'
 import Card from './componentes/Card'
+import { useEffect, useState } from 'react'
 
 function App() {
-  
+  const [dados, setDados] = useState([]);
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/MonicaHillman/codeconnect-api/publicacoes")
+    .then(resposta => resposta.json())
+    .then(dados => setDados(dados))
+  }, []) 
 
-  return (
+   return (
     <div className='container'>
     <Sidebar />
     <div>
     <BarraDePesquisa />
     <Filtro />
     <Ordenacao />
-    <Card />
+    <ul className='lista-cards'>
+      {dados ? dados.map((item, index) => (
+        <li key={index}>
+          <Card 
+          id = {item.id}
+          imagemUrl = {item.imagem_capa}
+          titulo={item.titulo}
+          resumo={item.resumo}
+          linhasDeCodigo = {item.linhas_de_codigo}
+          compartilhamentos = {item.compartilhamentos}
+          comentarios =  {item.comentarios}
+          usuario = {item.usuario}
+          />
+        </li>
+      )) : null}      
+    </ul>
     </div>
     </div>
   )
--- src/componentes/Card/index.jsx
+++ src/componentes/Card/index.jsx
@@ -1,37 +1,36 @@
-import Capa from './assets/capa.png'
 import Code from './assets/code.svg'
 import Chat from './assets/chat.svg'
 import Share from './assets/share.svg'
-import Icone from './assets/icone.png'
+
 import './styles.css'
 
-export default function Card(){
+export default function Card({ id, imagemUrl, titulo, resumo, linhasDeCodigo, compartilhamentos, comentarios, usuario }) {
     return (
         <article className="card">
             <div className="card__imagem">
-                <img src={Capa}/>
+                <img src={imagemUrl}/>
             </div>
 
                 <div className="card__conteudo">
                     <div className="conteudo__texto">
-                        <h3>Título do Post</h3>
-                        <p>Resumo do Post</p>
+                        <h3>{titulo}</h3>
+                        <p>{resumo}</p>
                     </div>
                     <div className="conteudo__rodape">
                         <ul>
                             <li>
-                                <img src={Code} alt="Códigos"/>100
+                                <img src={Code} alt="Códigos"/> {linhasDeCodigo}
                             </li>
                             <li>
-                                <img src={Share} alt="Compartilhamentos"/>12
+                                <img src={Share} alt="Compartilhamentos"/>{compartilhamentos}
                             </li>
                             <li>
-                                <img src={Chat} alt="Comentários"/>30
+                                <img src={Chat} alt="Comentários"/> {comentarios}
                             </li>
                         </ul>
                         <div className="rodape__usuario">
-                        <img src={Icone} alt='imagem do usuário' />
-                        @Matheus
+                        <img src={usuario.imagem} alt='imagem do usuário' />
+                        {usuario.nome}
                         </div>
                     </div>
                 </div>
