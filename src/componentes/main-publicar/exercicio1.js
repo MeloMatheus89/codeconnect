a376349fcab4efc48407d2144f0983ba8c6f24ca
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-03-03 10:07:13 -0300

    exercício 1 concluído

---
+++ src/componentes/main-publicar/exercicio1.js
@@ -0,0 +1,23 @@
+// Importa o botão de upload
+const uploadBtn = document.getElementById("upload-btn");
+// Importa a imagem de exemplo
+const inputUpload = document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click", () => {
+  inputUpload.click();
+});
+
+function lerConteudoDoArquivo(arquivo) {
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.name });
+    };
+    leitor.onerror = () => {
+      reject(`Erro na leitura do arquivo ${arquivo.name}`);
+    };
+
+    leitor.readAsDataURL(arquivo);
+  });
+}
